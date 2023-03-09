14c179e2fb782b0bdff7fc038701607870f5e34e
Shannon Feeley <scfeeley@wpi.edu>
2023-03-09 12:42:41 -0700

    User Interface
    Created a user interface

--- game.js
+++ game.js
@@ -1,79 +1,210 @@
+//Running score variables 
+let playerWins = 0;
+let compWins = 0; 
+let totalWins = 0;
+let totalGames = 0;
+let numWinsToEnd = 5; //number of wins to end round
 
 //Gets computer choice for rock,paper,scissors
 function getComputerChoice(){
     let num = Math.ceil(Math.random() * 3 );
     if(num === 1){
+        compRock.classList.add('chosen');
         return "Rock";
     }else if(num === 2){
+        compPaper.classList.add('chosen');
         return "Paper";
     }else{
+        compScissors.classList.add('chosen');
         return "Scissors";
     }
 }
+
+//Updates the score for the round
+function updateScore(player, comp){
+    playerScore.textContent = player;
+    compScore.textContent = comp;
+}
+
+//Updates the running total 
+function updateTotal(player, comp){
+    total.textContent = `${playerName.textContent} won ${totalWins} of ${totalGames} games`;
+    container.appendChild(total);
+}
+//Resets round
+function resetRound(){
+    playerWins = 0; 
+    compWins = 0; 
+    updateScore(0,0);
+}
+
+//Resets Game 
+function resetGame(){
+    resetRound();
+    totalWins = 0;
+    totalGames = 0;
+    message.textContent ="Let's Play!";
+    total.textContent = `You have won ${totalWins} of ${totalGames} games`;
+    container.appendChild(total);
+    resultContainer.appendChild(message);
+}
+//Checks to see if the round is over 
+function isRoundOver(num){
+    if(playerWins === num){
+        message.textContent = 'YOU WON!';
+        totalWins++;
+        totalGames++;
+    }else if(compWins === num){
+        message.textContent ='YOU LOST! Try again!';
+        totalGames++;
+    }else{
+        return;
+    }
+    resultContainer.appendChild(message);
+    updateTotal(totalWins, totalGames);
+    resetRound();
+}
+
+//Checks that transition has ended 
+function removeTransition(e){
+     console.log(e);
+    if(e.propertyName !== 'fill'){
+     return; 
+    }
+    this.classList.remove('chosen');
+   }
+
 //Plays one round of rock,paper,scissors given the player and computer selections
-//returns -1 if computer wins, 0 if tie, 1 if player wins
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
-    let message;
-    let result;
-    if(playerSelection === 'ROCK'){
-        if(computerSelection === 'PAPER'){
-            message = "You Lose! Paper beats Rock";
-            result = -1;
-        }else if(computerSelection === 'Rock'){
-            message ="You tied!";
-            result = 0;
-        }else{
-            message = "You Win! Rock beats Scissors!";
-            result = 1;
-        }
-    }else if(playerSelection === 'PAPER'){
-        if(computerSelection === 'ROCK'){
-            message = "You Win! Paper beats Rock";
-            result = 1;
-        }else if(computerSelection === 'PAPER'){
-            message = "You tie!";
-            result = 0;
-        }else{
-            message = "You Lose! Scissors beats Paper!";
-            result = -1;
-        }
-    }else{
-        if(computerSelection === 'ROCK'){
-            message = "You Lose! Rock beats Scissors";
-            result = -1;
-        }else if(computerSelection === 'SCISSORS'){
-            message =  "You tie!";
-            result = 0;
-        }else{
-            message = "You Win! Scissors beats Paper!";
-            result = 1;
-        }
+
+    //tie 
+    if(playerSelection === computerSelection){
+        message.textContent ='Tie!';
     }
-    console.log(message);
-    return result;
-}
-//Plays 5 rounds of rock,paper,scissors
-function game(){
-    let playerWins = 0;
-    let compWins = 0; 
-    for(let i = 0; i < 5; i++){
-        let playerChoice = window.prompt('Choose rock, paper, or scissors');
-        let result = playRound(playerChoice, getComputerChoice());
-        if(result === 1){
-            playerWins++;
-        }else if(result === -1){
-            compWins++;
+    //player wins 
+    else if((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') || 
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')){
+            updateScore(playerWins + 1, compWins);
+        playerWins++;
+        message.textContent ='Win!';
         }
+    //comp wins 
+    else {
+        compWins++;
+        updateScore(playerWins, compWins);
+        message.textContent ='Loss!';
     }
-    if(playerWins > compWins){
-        console.log("You win!");
-    }else if(compWins > playerWins){
-        console.log("You lose!");
-    }else{
-        console.log("You tied!");
-    }
+    //display result to UI 
+    resultContainer.appendChild(message);
+
+    //check to see if the round is over 
+    isRoundOver(numWinsToEnd);
 }
 
-game();
+/*****************************************************************
+USER INTERFACE  
+******************************************************************/
+const container = document.querySelector('.container');
+
+//Player game components 
+const playerGame = document.createElement('div');
+playerGame.classList.add('choices');
+
+const playerName = document.createElement('div');
+playerName.textContent = window.prompt("Please Enter Your Name", "Bob");
+//if player does not enter name, use player 1
+if(playerName.textContent === ''){
+    playerName.textContent = 'Player 1';
+} 
+
+const rock = document.querySelector('#playerRock');
+rock.classList.add('player');
+
+const paper = document.querySelector('#playerPaper');
+paper.classList.add('player');
+
+const scissors = document.querySelector('#playerScissors');
+scissors.classList.add('player');
+
+const playerScore = document.createElement('div');
+playerScore.textContent = '0';
+playerScore.classList.add('score');
+
+playerGame.appendChild(playerName);
+playerGame.appendChild(rock);
+playerGame.appendChild(paper);
+playerGame.appendChild(scissors);
+playerGame.appendChild(playerScore);
+
+//Computer game components 
+const compGame = document.createElement('div');
+compGame.classList.add('choices');
+
+const computer = document.createElement('div');
+computer.textContent ='Computer';
+
+const compRock = document.querySelector('#compRock');
+
+const compPaper = document.querySelector('#compPaper');
+
+const compScissors = document.querySelector('#compScissors');
+
+const compScore = document.createElement('div');
+compScore.textContent = '0';
+compScore.classList.add('score');
+
+compGame.appendChild(computer);
+compGame.appendChild(compRock);
+compGame.appendChild(compPaper);
+compGame.appendChild(compScissors);
+compGame.appendChild(compScore);
+
+//Game Board 
+const gameContainer = document.createElement('div');
+gameContainer.classList.add('game');
+gameContainer.appendChild(playerGame);
+gameContainer.appendChild(compGame);
+
+//Round Score and Game Reset Display
+const resultContainer = document.createElement('div');
+resultContainer.classList.add("result");
+
+const reset = document.createElement('button');
+reset.textContent = 'RESET';
+reset.classList.add('reset');
+const message = document.createElement('p');
+message.textContent = "Let's Play!";
+resultContainer.appendChild(reset);
+resultContainer.appendChild(message);
+
+container.appendChild(gameContainer);
+container.appendChild(resultContainer);
+
+//Total Score Display 
+const total = document.createElement('div');
+total.textContent = `${playerName.textContent} won ${totalWins} of ${totalGames} games`;
+total.classList.add('total');
+container.appendChild(total);
+
+//checks that color transition has happened to show user and computer selection 
+const keys = document.querySelectorAll('svg');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+/*****************************************************************
+Event Listeners
+******************************************************************/
+rock.addEventListener('click', () => {
+    playRound('rock', getComputerChoice());
+});
+paper.addEventListener('click', () => {
+    playRound('paper', getComputerChoice());
+});
+scissors.addEventListener('click', () => {
+        playRound('scissors', getComputerChoice());
+});
+reset.addEventListener('click', () =>{
+    resetGame();
+})
